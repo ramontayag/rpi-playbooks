eca01af4c5ff78497e2e2e02477a8493c516d654
Ramon Tayag <ramon.tayag@gmail.com>
2014-09-30 19:51:20 +0800

    Update btsync

--- roles/btsync/files/install.sh
+++ roles/btsync/files/install.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/Downloads/btsync
 cd ~/Downloads/btsync
-wget http://btsync.s3-website-us-east-1.amazonaws.com/btsync_arm.tar.gz
+wget http://download-new.utorrent.com/endpoint/btsync/os/linux-arm/track/stable
 tar zxvf btsync_arm.tar.gz
 cd ~/Downloads
 mkdir ~/local
