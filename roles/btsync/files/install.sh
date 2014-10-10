cefcb4b59a27a9d63ebfbd18755ec52fb964274a
Ramon Tayag <ramon.tayag@gmail.com>
2014-10-10 21:13:48 +0800

    Move RVM into own role; update btsync

--- roles/btsync/files/install.sh
+++ roles/btsync/files/install.sh
@@ -1,8 +1,9 @@
 mkdir -p ~/Downloads/btsync
 cd ~/Downloads/btsync
 wget http://download-new.utorrent.com/endpoint/btsync/os/linux-arm/track/stable
+mv stable btsync_arm.tar.gz
 tar zxvf btsync_arm.tar.gz
 cd ~/Downloads
-mkdir ~/local
+mkdir -p ~/local
 mv btsync ~/local
 ~/local/btsync/btsync --dump-sample-config > ~/local/btsync/btsync.conf
