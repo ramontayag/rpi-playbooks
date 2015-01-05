2471f8225f151de762991f768fd1478c24887f6f
Ramon Tayag <ramon.tayag@gmail.com>
2015-01-05 18:41:47 +0800

    Update btsync installer

--- roles/btsync/files/install.sh
+++ roles/btsync/files/install.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/Downloads/btsync
 cd ~/Downloads/btsync
-wget http://download-new.utorrent.com/endpoint/btsync/os/linux-arm/track/stable
+wget http://download.getsyncapp.com/endpoint/btsync/os/linux-arm/track/stable
 mv stable btsync_arm.tar.gz
 tar zxvf btsync_arm.tar.gz
 cd ~/Downloads
